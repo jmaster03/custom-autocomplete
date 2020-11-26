f39d9f9a5265e94ff8fbe38b7aa078a7d8f7e315
Jesus  Medrano <46202461+jmaster03@users.noreply.github.com>
2020-11-26 17:48:53 -0400

    Update dropdown.directive.ts

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -5,13 +5,13 @@ import { Directive, ElementRef, Output, EventEmitter, HostListener } from '@angu
 })
 export class ClickOutsideDirective {
     @Output() public clickOutside = new EventEmitter();
-    constructor(private _elementRef: ElementRef) {
+    constructor(private el: ElementRef) {
 
     }
 
     @HostListener('document:click', ['$event.target'])
     public onClick(targetElement) {
-        const isClickedInside = this._elementRef.nativeElement.contains(targetElement);
+        const isClickedInside = this.el.nativeElement.contains(targetElement);
         if (!isClickedInside) {
             this.clickOutside.emit(null);
         }
